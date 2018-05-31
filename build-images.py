78bd474d7d21290001aaaac5795d17b742ba3077
Hawken Rives <rives@stolaf.edu>
2018-05-30 21:45:23 -0500

    actually build the images

--- build-images.py
+++ build-images.py
@@ -30,14 +30,60 @@ def discover_images():
 					tomcat_base_image == matrix['defaults']['TOMCAT_BASE']
 
 				if is_default_tomcat:
-					alternate_tags.append(f"lucee/lucee:{lucee_version}{lucee_variant}{lucee_server}")
+					alternate_tags.append(f"{lucee_version}{lucee_variant}{lucee_server}")
 				if is_default_tomcat and (not lucee_variant and not lucee_server):
-					alternate_tags.append(f"lucee/lucee:{get_minor_version(lucee_version)}")
+					alternate_tags.append(f"{get_minor_version(lucee_version)}")
 
 				yield {
-					'primary_tag': f"lucee/lucee:{lucee_version}{lucee_variant}{lucee_server}-tomcat{tomcat_version}-{tomcat_java_version}{tomcat_base_image}",
+					'primary_tag': f"{lucee_version}{lucee_variant}{lucee_server}-tomcat{tomcat_version}-{tomcat_java_version}{tomcat_base_image}",
 					'alternate_tags': alternate_tags,
+					'base_tag': f"lucee:{lucee_version}-tomcat{tomcat_version}-{tomcat_java_version}{tomcat_base_image}",
+					'config': {
+						'LUCEE_VERSION': lucee_version,
+						'LUCEE_SERVER': lucee_server,
+						'LUCEE_VARIANT': lucee_variant,
+						'TOMCAT_VERSION': tomcat_version,
+						'TOMCAT_JAVA_VERSION': tomcat_java_version,
+						'TOMCAT_BASE_IMAGE': tomcat_base_image,
+					}
 				}
 
 for image in discover_images():
-	print(image)
+	# print(image)
+
+	minor = get_minor_version(image['config']['LUCEE_VERSION'])
+
+	build_args = {
+		**image['config'],
+		'LUCEE_MINOR': minor,
+		'LUCEE_IMAGE': image['base_tag'],
+	}
+	build_args = [arg for key, value in build_args.items() for arg in ['--build-arg', f"{key}={value}"]]
+
+	tags = [image['primary_tag'], *image['alternate_tags']]
+	tags = [arg for value in tags for arg in ['-t', f"kryestofer/lucee:{value}"]]
+
+	if not image['config']['LUCEE_SERVER'] and not image['config']['LUCEE_VARIANT']:
+		tags.append(f"-t {image['base_tag']}")
+
+	if image['config']['LUCEE_SERVER'] == '-nginx':
+		if image['config']['TOMCAT_BASE_IMAGE'] == '-alpine':
+			dockerfile = './Dockerfile.nginx.alpine'
+		else:
+			dockerfile = './Dockerfile.nginx'
+	else:
+		dockerfile = './Dockerfile'
+
+	command = [
+		"docker", "build",
+		*build_args,
+		"-f", dockerfile,
+		*tags,
+		".",
+	]
+
+	print(' '.join(command))
+	subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	for tag in [tag for tag in tags if tag.startswith('kryestofer/')]:
+		subprocess.run(["docker", "push", tag], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
